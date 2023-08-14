bdd2664c14c92b07ae35338275fa25a9e2f329a9
Abid Hasan <abidtkg@gmail.com>
2023-08-15 04:27:06 +0600

    error page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./modules/site/site.module').then(x => x.SiteModule) }
+  { path: '', loadChildren: () => import('./modules/site/site.module').then(x => x.SiteModule) },
+  { path: '**', component: ErrorPageComponent }
 ];
 
 @NgModule({
